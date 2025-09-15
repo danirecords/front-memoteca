8279cf8f522d33d776d9d991d9f33b9ed0682a07
daniel.oliveira <contato@daniweb.com.br>
2025-09-15 07:57:41 -0300

    #DW-01 - feat(angular): adicionando formulário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from "./componentes/rodape/rodape.component";
+import { CriarPensamentoComponent } from "./componentes/pensamentos/criar-pensamento/criar-pensamento.component"
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
