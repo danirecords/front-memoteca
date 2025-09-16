ceaa83acf18eb6f020f360aa3fb0937b9e1f299b
daniel.oliveira <contato@daniweb.com.br>
2025-09-16 19:10:16 -0300

    #DW-01 - feat(angular): projeto de estudos memoteca

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './criar-pensamento.component.html',
   styleUrl: './criar-pensamento.component.css'
 })
 export class CriarPensamentoComponent {
+
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo angular',
+    autoria: 'Dev',
+    modelo: 'modelo1'
+  }
+
+  criarPensamento(){
+    alert('Novo pensamento criado!')
+  }
+
+  cancelar(){
+    alert('Cancelar pensamento!')
+  }
 
 }
