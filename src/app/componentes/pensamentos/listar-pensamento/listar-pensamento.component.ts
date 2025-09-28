d40e28b56fb2d21bafbbf6e27fac869928534f05
daniel.oliveira <contato@daniweb.com.br>
2025-09-27 22:56:44 -0300

    #DW-01 - feat(angular): utilizando a diretiva ngClass

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,18 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { PensamentoComponent } from "../pensamento/pensamento.component";
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-listar-pensamento',
   standalone: true,
-  imports: [RouterLink, PensamentoComponent],
+  imports: [RouterLink, PensamentoComponent, NgForOf],
   templateUrl: './listar-pensamento.component.html',
   styleUrl: './listar-pensamento.component.css'
 })
 
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [];
+   listaPensamentos = [
+      {
+        conteudo: 'Comunicação entre componentes',
+        autoria: 'Angular',
+        modelo: 'modelo3'
+      },
+      {
+        conteudo: 'Comunicação entre componentes 2',
+        autoria: 'Angular 2',
+        modelo: 'modelo2'
+      },
+      {
+        conteudo: 'O treinador também destacou o momento vivido por Philippe Coutinho - substituído no segundo tempo aos gritos de "Coutinho é Seleção" da arquibancada. Na opinião de Diniz, o meia tem entrado no radar do técnico Carlo Ancelotti e contrariado as expectativas à medida que envelhece.',
+        autoria: 'Angular 2',
+        modelo: 'modelo1'
+      }
+  ];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
@@ -10,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 
 export class PensamentoComponent implements OnInit {
 
-  pensamento = {
+  @Input() pensamento = {
     conteudo: 'I Love angular',
     autoria: 'Nay',
     modelo: 'modelo3'
@@ -20,6 +20,13 @@ export class PensamentoComponent implements OnInit {
 
   ngOnInit(): void {
 
+  }
+
+  larguraPensamento(): string {
+    if(this.pensamento.conteudo.length >= 259){
+      return 'pensamento-g'
+    }
+    return 'pensamento-p'
   }
 
 }
